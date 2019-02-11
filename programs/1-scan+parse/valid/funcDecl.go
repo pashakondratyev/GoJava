3938ad7ac8755d708656517c924b8675c68ab511
kkutschera <kayleekutschera@gmail.com>
2019-02-11 10:53:28 -0500

    Add test programs

---
+++ programs/1-scan+parse/valid/funcDecl.go
@@ -0,0 +1,10 @@
+// valid function declarations
+package test
+
+func f(a int, b int) int {
+	return a;
+}
+
+func g(a, b int) int {
+	return a;
+}
---
+++ programs/1-scan+parse/valid/typeDecl.go
@@ -0,0 +1,11 @@
+// valid type declarations
+package test
+
+type natural int
+type real float64
+
+type (
+	point struct {
+		x, y, z real
+	}
+)
