f6b702b2dd8026766744ca95ca82e453fa753a6a
kkutschera <kayleekutschera@gmail.com>
2019-02-12 20:42:33 -0500

    Adjust valid test

--- programs/1-scan+parse/valid/switch.go
+++ programs/1-scan+parse/valid/switch.go
@@ -4,6 +4,11 @@ package test
 x := 6
 
 switch x {
-	case 0,1,2,3: println("small")
-	case 4,5,6,7: println("large")
+case 0,1,2,3: 
+	println("small")
+case 4,5,6,7: 
+	println("large")
+	fallthrough
+case 6,7,8,9: 
+	println("very large")
 }
