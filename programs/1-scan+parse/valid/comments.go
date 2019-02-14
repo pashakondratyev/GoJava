54e3879cd72c89b1966cf862e112895ed15f08bc
kkutschera <kayleekutschera@gmail.com>
2019-02-14 09:55:04 -0500

    Add another small valid programs

---
+++ programs/1-scan+parse/valid/comments.go
@@ -0,0 +1,12 @@
+// valid comments
+package test
+
+var x = 7
+/* block
+comment
+*/
+
+if x == 2 {
+	println("it is two") 	// this is a valid comment
+	println("or is it")		/* also a valid comment*/
+}
