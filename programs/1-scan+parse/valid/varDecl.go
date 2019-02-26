314d2a97a6ba20d17bceae639a3064e94779c1e1
kkutschera <kayleekutschera@gmail.com>
2019-02-26 10:02:53 -0500

    Add programs for blank identifier

--- programs/1-scan+parse/valid/varDecl.go
+++ programs/1-scan+parse/valid/varDecl.go
@@ -1,17 +1,24 @@
 // valid variable declarations
 package test
 
-var x1, x2 int
+var x1, x2 int = 44, 33
 var y int = 12
 var z = 24
 var _ = 5
 
 var (
-	a1, a2 int
+	a1, _ int = 13, 7
 	b int = 12
 	c = 24
 )
 
-i, j := 0, 10
-f := func() int { return 7 }
+
+func main() {
+	i, j := 0, 10
+	f := a() 
+}
+
+func a() {
+	return 7;
+}
 
