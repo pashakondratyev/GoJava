c3b4513d062b27e9c58856ba17c90a8c4d6aea5d
kkutschera <kayleekutschera@gmail.com>
2019-03-21 11:36:11 -0400

    Code gen expressions that are directly translated to Java - comments left for sections that need to be fixed/completed

--- src/code.c
+++ src/code.c
@@ -15,6 +15,15 @@ void writeTab(int tabCount) {
   }
 }
 
+// prefix all identifiers
+char* prefix(char* str) {
+	char* prefix = "__golite__";
+	char* result = malloc((strlen(str)+strlen(prefix)+1)*sizeof(char));
+	sprintf(result, "%s%s", prefix, str);
+	return result;
+}
+
+
 int indexLastForwardSlash(char *str){
 	int index = -1;
   	for(int i = 0; i <= strlen(str); i++) {
@@ -30,8 +39,9 @@ void codeProgram(PROG *prog, SymbolTable *st, char *inputFileName) {
 	strncpy(outputFileName, inputFileName, strlen(inputFileName));
 	sprintf(outputFileName, "%s.java", inputFileName);
 	outputFile = fopen(outputFileName, "w");
+	free(outputFileName);
 	if (outputFile == NULL) {
- 		fprintf(stderr, "Can't open output file %s!\n", outputFileName);
+ 		fprintf(stderr, "Can't open output file!\n");
   		exit(1);
 	}
 
@@ -96,4 +106,221 @@ void codeTypeDecl(TYPE_SPECS *ts, SymbolTable *st, int tabCount) {
 
 void codeFuncDecl(FUNC_DECL *fd, SymbolTable *st, int tabCount) {
 	// TODO: implement
+}
+
+void codeStmt(STMT *stmt, SymbolTable *st, TYPE *returnType, int tabCount) {
+	// TODO: implement
+}
+
+void codeExp(EXP *exp, SymbolTable *st, int tabCount) {
+	// TODO: complete
+  	if (exp != NULL) {
+    	switch (exp->kind) {
+	      case ek_id:
+	       	if (strcmp(exp->val.id, "_") == 0) {
+	       		// TODO: do something with blank id
+	       		break;
+	       	}
+	       	fprintf(outputFile, "%s", prefix(exp->val.id));
+	       	break;
+	    	case ek_float:
+	    		fprintf(outputFile, "%f", exp->val.floatval);
+	        break;
+	      case ek_int:
+	        fprintf(outputFile, "%i", exp->val.intval);
+	        break;
+	      case ek_string:
+	        fprintf(outputFile, "%s", exp->val.stringval);
+	        break;
+	      case ek_boolean:
+	      	// TODO: check if want to use GoLite specific boolean values
+	        fprintf(outputFile, "%s", exp->val.booleanval ? "true" : "false");
+	        break;
+	      case ek_rune:
+	        fprintf(outputFile, "%c", exp->val.runeval);
+	        break;
+      	case ek_plus:
+      		// TODO: if char cast to char at the end
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " + ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+      	case ek_minus:
+      		fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " - ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+      	case ek_times:
+      		fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " * ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+      	case ek_div:
+      		fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " / ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+      	case ek_eq:
+      		// TODO: fix for strings, arrays, slices, and structs
+      		fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " == ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+     	 	case ek_ne:
+     	 		// TODO: fix for strings, arrays, slices, and structs
+        	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " != ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_ge:
+	      	// TODO: fix for strings
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " >= ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_le:
+	      	// TODO: fix for strings
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " <= ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_gt:
+	      	// TODO: fix for strings
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " > ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_lt:
+	      	// TODO: fix for strings
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " < ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_and:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " && ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_or:
+	     		fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " || ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_mod:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " %% ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitAnd:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " & ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitOr:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " | ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitXor:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " ^ ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitLeftShift:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " << ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitRightShift:
+	      	fprintf(outputFile, "(");
+	        codeExp(exp->val.binary.lhs, st, tabCount);
+	        fprintf(outputFile, " >> ");
+	        codeExp(exp->val.binary.rhs, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_bitClear:
+	      	//TODO: implement bit clearing in Java
+	      	break;
+
+	      case ek_uplus:
+	      	// TODO: must cast char back to a char
+	      	fprintf(outputFile, "(+");
+	        codeExp(exp->val.unary.exp, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+	      case ek_uminus:
+	      	// TODO: must cast char back to a char
+	      	fprintf(outputFile, "(-");
+	        codeExp(exp->val.unary.exp, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;
+     	 	case ek_bang:
+        	fprintf(outputFile, "(!");
+	        codeExp(exp->val.unary.exp, st, tabCount);
+	        fprintf(outputFile, ")");
+	        break;	
+      	case ek_ubitXor:
+        	//TODO: implement unary bit xor in Java
+      		break;
+      	case ek_func:
+        	// TODO: complete
+       		break;
+     		case ek_append:
+        	// TODO: complete
+       		break;
+      	case ek_len:
+        	// TODO: complete
+       		break;
+      	case ek_cap:
+       		// TODO: complete
+       		break;
+      	case ek_indexExp:
+        	// TODO: complete
+       		break;
+      	case ek_structField:
+        	// TODO: complete
+       		break;
+      	case ek_paren:
+	        // TODO: complete
+       		break;
+      	case ek_conv:
+        	// TODO: complete
+       		break;
+    }
+  }
+
 }
--- src/code.h
+++ src/code.h
@@ -5,17 +5,21 @@
 #include "symbol.h"
 
 void writeTab(int tabCount);
+char* prefix(char* str);
 
+int indexLastForwardSlash(char *str);
 void codeProgram(PROG *prog, SymbolTable *st, char *filename);
 void codeSetup();
 void codeComplete();
-void codePackage(PACKAGE *package);
 void codeDeclarations(DECL *dcl, SymbolTable *st, int tabCount);
-
 
 void codeVarDecl(VAR_SPECS *vs, SymbolTable *st, int tabCount);
 void codeShortDecl(SHORT_SPECS *ss, SymbolTable *st, int tabCount);
 void codeTypeDecl(TYPE_SPECS *ts, SymbolTable *st, int tabCount);
 void codeFuncDecl(FUNC_DECL *fd, SymbolTable *st, int tabCount);
+
+void codeStmt(STMT *stmt, SymbolTable *st, TYPE *returnType, int tabCount);
+void codeExp(EXP *exp, SymbolTable *st, int tabCount);
+
 
 #endif
