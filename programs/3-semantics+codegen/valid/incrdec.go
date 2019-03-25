0a7f883fde5ac0ec63678fe1f3fa3bcaad02cb5e
kkutschera <kayleekutschera@gmail.com>
2019-03-25 13:40:12 -0400

    Add increment/decrement codegen program

---
+++ programs/3-semantics+codegen/valid/incrdec.go
@@ -0,0 +1,22 @@
+//^2
+//^+6.600000e+000
+//^115
+
+// Increment/decrement
+package main
+
+func main() {
+	var a, b, c = 1, 5.6, 'r'
+	a++
+	a++
+	b++
+	b++
+	c++
+	c++
+	a--
+	b--
+	c--
+	println(a)
+	println(b)
+	println(c)
+}
