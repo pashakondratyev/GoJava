0cb1bdd6c4dda6421a784622270b8709aaee4d61
kkutschera <kayleekutschera@gmail.com>
2019-03-27 13:16:42 -0400

    Outline statement codegen

--- src/code.c
+++ src/code.c
@@ -394,8 +394,52 @@ void codeFuncDecl(FUNC_DECL *fd, SymbolTable *st, int tabCount) {
   fprintf(outputFile, "\t}\n");
 }
 
-void codeStmt(STMT *stmt, SymbolTable *st, TYPE *returnType, int tabCount) {
+void codeStmt(STMT *stmt, SymbolTable *st, int tabCount) {
   // TODO: implement
+  if (stmt != NULL) {
+  	switch (stmt->kind) {
+  		case sk_block:
+  			break;
+  		case sk_exp:
+  			break;
+  		case sk_assign:
+  			break;
+  		case sk_assignOp:
+  			break;
+  		case sk_decl:
+  			break;
+  		case sk_shortDecl:
+  			break;
+  		case sk_incr:
+  			break;
+  		case sk_decr:
+  			break;
+  		case sk_print:
+  			break;
+  		case sk_println:
+  			break;
+  		case sk_return:
+  			break;
+  		case sk_if:
+  			break;
+  		case sk_else:
+  			break;
+  		case sk_switch:
+  			break;
+  		case sk_for:
+  			break;
+  		case sk_break:
+  			break;
+  		case sk_continue:
+  			break;
+  		case sk_fallthrough:
+  			fprintf(stderr, "Error: fallthough not supported.\n");
+  			break;
+  		case sk_empty:
+  			// empty statement
+  			break;
+  	}
+  }
 }
 
 void codeExp(EXP *exp, SymbolTable *st, int tabCount) {
