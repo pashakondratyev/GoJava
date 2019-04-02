97ec7f6d9d23ca48735f25d06e9690ac43d2f103
Kabilan Sriranjan <kabilan.s97@gmail.com>
2019-04-02 10:38:02 -0400

    testing assignment statements

---
+++ programs/3-semantics+codegen/valid/assignments2.go
@@ -0,0 +1,31 @@
+//~1
+//~hello
+//~hello
+//~+1.000000e+00
+
+package test
+
+var i int
+var s string
+var b bool
+var f float64
+
+func main() {
+  i := int(f)
+  s := s
+  var b = "hello"
+  s = b
+  f = float64(i)
+
+  println(i)
+  println(s)
+  println(b)
+  println(f)
+}
+
+func init() {
+  i = 3
+  s = "hi"
+  b = false
+  f = 1.1
+}
--- programs/3-semantics+codegen/valid/switchStmts2.go
+++ programs/3-semantics+codegen/valid/switchStmts2.go
@@ -50,9 +50,9 @@ func baz() {
 }
 
 func wag() {
-  switch t:=10 {
+  switch t:=10; {
     case t>0:
-      switch s:=5 {
+      switch s:=5; {
         case s>t:
           println("NONONO")
         default:
