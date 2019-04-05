64f912d60c3cf3137f69c771ee761c0062c23ce8
kkutschera <kayleekutschera@gmail.com>
2019-04-05 11:12:28 -0400

    Move "invalid" programs over to valid. They are valid programs, but they do result in errors.

--- programs/3-semantics+codegen/valid/verify.sh
+++ programs/3-semantics+codegen/valid/verify.sh
@@ -3,7 +3,7 @@
 IFS=
 USER_OUTPUT=$(./execute.sh $1 2>&1)
 EXIT_CODE=${PIPESTATUS[0]}
-USER_OUTPUT=$(echo "$USER_OUTPUT" | gsed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9][0-9])([^0-9]|$)/\10\2/g' | sed 's/\r//g' | sed 's/\\/\\\\/')
+USER_OUTPUT=$(echo "$USER_OUTPUT" | sed -r 's/([+\-][0-9]+\.[0-9]+e[+\-])([0-9][0-9])([^0-9]|$)/\10\2/g' | sed 's/\r//g' | sed 's/\\/\\\\/')
 EXPECTED_OUTPUT=$(grep "//~" $1 | sed 's/\/\/~//' | sed 's/\r//g' | sed 's/\\/\\\\/')
 
 SHOULD_ERROR=$(grep "//!" $1 | wc -l)
